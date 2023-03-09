0aa841ddef9a5d39fb9feb243d7b5d2cecfcf36c
gks93 <gks930620@naver.com>
2023-03-09 17:43:14 +0900

    redis 정리

--- src/test/java/com/prac/prac/PracApplicationTests.java
+++ src/test/java/com/prac/prac/PracApplicationTests.java
@@ -1,13 +1,9 @@
 package com.prac.prac;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PracApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
